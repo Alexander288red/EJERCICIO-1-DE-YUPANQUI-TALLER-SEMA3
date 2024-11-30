f73adff476903571a96fdbfae89758e876346fe8
Alexander Yupanqui Reyes <u00030823@sise.com.pe>
2024-11-29 23:05:31 -0500

    EJERCICIO 1 YUPANQUI

---
+++ src/main/java/com/example/EJERCICIO1/YUPANQUI/Ejercicio1YupanquiApplication.java
@@ -0,0 +1,13 @@
+package com.example.EJERCICIO1.YUPANQUI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio1YupanquiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicio1YupanquiApplication.class, args);
+	}
+
+}
